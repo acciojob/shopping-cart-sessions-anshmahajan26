90fb87f2dd9bdd490a7094f26ebf44003d495292
Ansh Mahajan <anshmahajan678@gmail.com>
2026-02-01 14:01:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// This is the boilerplate code given for you
-// You can modify this code
-// Product data
+const productList = document.getElementById("product-list");
+const cartList = document.getElementById("cart-list");
+const clearCartBtn = document.getElementById("clear-cart-btn");
+
+// Product data (given)
 const products = [
   { id: 1, name: "Product 1", price: 10 },
   { id: 2, name: "Product 2", price: 20 },
@@ -9,29 +11,46 @@ const products = [
   { id: 5, name: "Product 5", price: 50 },
 ];
 
-// DOM elements
-const productList = document.getElementById("product-list");
+// Load cart from sessionStorage
+let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
 
-// Render product list
+// Render products
 function renderProducts() {
   products.forEach((product) => {
     const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    li.innerText = `${product.name} - $${product.price} `;
+
+    const btn = document.createElement("button");
+    btn.innerText = "Add to Cart";
+
+    btn.addEventListener("click", () => {
+      cart.push(product);
+      sessionStorage.setItem("cart", JSON.stringify(cart));
+      renderCart();
+    });
+
+    li.appendChild(btn);
     productList.appendChild(li);
   });
 }
 
-// Render cart list
-function renderCart() {}
+// Render cart
+function renderCart() {
+  cartList.innerHTML = "";
 
-// Add item to cart
-function addToCart(productId) {}
-
-// Remove item from cart
-function removeFromCart(productId) {}
+  cart.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerText = `${item.name} - $${item.price}`;
+    cartList.appendChild(li);
+  });
+}
 
 // Clear cart
-function clearCart() {}
+clearCartBtn.addEventListener("click", () => {
+  cart = [];
+  sessionStorage.removeItem("cart");
+  renderCart();
+});
 
 // Initial render
 renderProducts();
